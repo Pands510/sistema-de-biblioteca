ea67de21ce3aa21afb9938294c11a4eeccc32258
Pands510 <capaivaramdl@gmail.com>
2026-07-02 01:24:34 -0300

    classe Pessoa implementada

--- Pessoa.java
+++ Pessoa.java
@@ -1,18 +1,22 @@
-//Responsabilidades:
-    //Representar qualquer pessoa da biblioteca
-    //Não sabe cadastrar livros
-    //Não sabe emprestar
-    //Apenas guarda informações pessoais
+/*Responsabilidades:
+    Representar qualquer pessoa da biblioteca
+    Não sabe cadastrar livros
+    Não sabe emprestar
+    Apenas guarda informações pessoais
+*/
 
-public abstract class Pessoa {
+public abstract class Pessoa { //Representa uma Pessoa que vai interagir com o sistema
     private String nome;
-    private String cpf;
+    private final String cpf; //Imutabilidade do CPF
     private String tel;
 
     public Pessoa(String nome, String cpf, String tel) {
         this.nome = nome;
+        if(!validarCpf(cpf)){
+            throw new IllegalArgumentException("CPF inválido");
+        }
         this.cpf = cpf;
-        this.tel = tel;
+        setTel(tel);
     }
 
     public String getNome() {
@@ -20,6 +24,12 @@ public abstract class Pessoa {
     }
 
     public void setNome(String nome) {
+        if(nome == null){
+            throw new IllegalArgumentException("nome invalido");
+        }
+        if(nome.isBlank()){
+            throw new IllegalArgumentException("nome invalido");
+        }
         this.nome = nome;
     }
 
@@ -27,16 +37,23 @@ public abstract class Pessoa {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
     public String getTel() {
         return tel;
     }
 
     public void setTel(String tel) {
-        this.tel = tel;
+        if(tel.length() == 11){
+            this.tel = tel;
+        }
+    }
+
+    private boolean validarCpf(String cpf){
+
+        if(cpf == null){
+            return false;
+        }
+
+        return cpf.matches("\\d{11}");
     }
 
     @Override
